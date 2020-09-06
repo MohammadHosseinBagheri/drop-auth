eada02645d790782d115f831c3d7b461453328c3
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2020-09-07 01:57:16 +0430

    Login page

--- src/routes/auth/login/Login.js
+++ src/routes/auth/login/Login.js
@@ -1,14 +1,36 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Dimensions} from 'react-native';
+import HeaderTitle from '../../../components/login/HeaderTitle';
+import LoginContent from '../../../components/login/LoginContent';
+
+const {height} = Dimensions.get('window');
 
 const Login = () => {
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={styles.container}>
+      <HeaderTitle />
+      <View style={styles.content}>
+        <Text style={{fontSize: 18, fontWeight: 'bold'}}>Welcome</Text>
+        <LoginContent />
+      </View>
     </View>
   );
 };
 
 export default Login;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ECEFF1',
+  },
+
+  content: {
+    flex: 0.8,
+    backgroundColor: '#fff',
+    borderTopRightRadius: 40,
+    borderTopLeftRadius: 40,
+    paddingTop: (height * 5) / 100,
+    paddingHorizontal: 20,
+  },
+});
