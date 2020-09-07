c18ebf5c008d37a68cf50462f603f5973a550a4a
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2020-09-07 23:35:55 +0430

    Add redux config

--- App.js
+++ App.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import rootReducer from './src/redux/index';
 import Config from './src/routes';
+import logger from 'redux-logger';
+
+const middlewares = [logger];
+const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
 
 const App = () => {
-  return <Config />;
+  return (
+    <Provider store={store}>
+      <Config />
+    </Provider>
+  );
 };
 
 export default App;
