c06e00cccd88f1b9bd2455dcedd78cd26f0f1370
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2020-09-08 01:56:29 +0430

    Header component and homecontent component

--- src/routes/home/home.js
+++ src/routes/home/home.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import HeaderContent from '../../components/home/HeaderContent';
+import HomeContent from '../../components/home/HomeContent';
 
-const Home = () => {
+const Home = (props) => {
+  const {navigation} = props;
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <HeaderContent />
+      <HomeContent navigation={navigation} />
     </View>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ECEFF1',
+  },
+});
