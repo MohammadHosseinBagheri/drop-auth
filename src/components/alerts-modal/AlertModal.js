753503a2d661f32185db79a137a2a340ef8aa39b
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2020-09-07 23:36:12 +0430

    Alert modal

---
+++ src/components/alerts-modal/AlertModal.js
@@ -0,0 +1,71 @@
+import React, {Component} from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+import Modal from 'react-native-modalbox';
+
+export class AlertModal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+    };
+  }
+  render() {
+    const {navigation} = this.props;
+    this.open = async (text) => {
+      this.setState({ text: text });
+      await this.refs.alertModal.open();
+      setTimeout(async () => {
+            await this.close();
+            if (this.state.text == 'success') {
+                this.homeScreen();
+            }
+        }, 2000);
+    };
+    this.close = () => this.refs.alertModal.close();
+    this.homeScreen = () => navigation.replace('home');
+    return (
+      <Modal
+        ref={'alertModal'}
+        position={'bottom'}
+        backdrop={false}
+        style={[
+          styles.modal,
+          {
+            backgroundColor:
+              this.state.text == 'success' ? 'seagreen' : '#d50000',
+          },
+        ]}>
+        <View style={styles.content}>
+          <Text style={styles.text}>{this.state.text}</Text>
+        </View>
+      </Modal>
+    );
+  }
+}
+
+export default AlertModal;
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    borderRadius: 15,
+    justifyContent: 'center',
+  },
+  modal: {
+    height: 60,
+    //   position: 'absolute',
+    //   botton:50,
+    borderRadius: 15,
+    width: '95%',
+    marginBottom: 100,
+    justifyContent: 'center',
+  },
+  text: {
+    textAlign: 'left',
+    marginHorizontal: 20,
+    fontSize: 16,
+    color: 'white',
+    fontWeight: 'bold',
+    alignItems: 'center',
+  },
+});
