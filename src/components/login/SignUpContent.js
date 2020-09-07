04ddd814996b612dd65fbf7214e4e19b85854f46
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2020-09-08 01:55:54 +0430

    Navigate to register screen

--- src/components/login/SignUpContent.js
+++ src/components/login/SignUpContent.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {Button} from 'native-base';
-const SignUpContent = () => {
+const SignUpContent = (props) => {
+  const {navigation} = props;
+
+  const registerScreen = () => {
+    navigation.replace('register');
+  };
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Don't have an account?</Text>
-      <Button transparent style={{marginHorizontal: 5}}>
+      <Button
+        transparent
+        style={{marginHorizontal: 5}}
+        onPress={() => registerScreen()}>
         <Text style={styles.buttonText}>Sign up</Text>
       </Button>
     </View>
