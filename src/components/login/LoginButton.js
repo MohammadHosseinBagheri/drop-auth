7ea6ee0ddd8b76ddb6522541dc7e3f01b1b8f073
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2020-09-07 23:38:22 +0430

    On press method

--- src/components/login/LoginButton.js
+++ src/components/login/LoginButton.js
@@ -2,9 +2,10 @@ import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
 import {Button} from 'native-base';
-const LoginButton = () => {
+const LoginButton = (props) => {
+  const {onPress} = props;
   return (
-    <Button full style={styles.button}>
+    <Button onPress={() => onPress()} full style={styles.button}>
       <Text style={styles.text}>Log in</Text>
     </Button>
   );
