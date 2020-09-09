439685d487fa414e6533660600121a4cfe37ed9d
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2020-09-09 21:29:03 +0430

    force ltr

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, I18nManager, AsyncStorage} from 'react-native';
 import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import rootReducer from './src/redux/index';
 import Config from './src/routes';
 import logger from 'redux-logger';
+import RNRestart from 'react-native-restart';
+
+AsyncStorage.setItem('lang', 'ar', () => {
+  AsyncStorage.getItem('lang', (value) => {
+    if (value != null) {
+      I18nManager.forceRTL(false);
+      RNRestart.Restart();
+    }
+  });
+});
 
 const middlewares = [logger];
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
-
 
 const App = () => {
   return (
