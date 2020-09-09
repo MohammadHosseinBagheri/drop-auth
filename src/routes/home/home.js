3e4b550c2b252d329d1d3332b32ef4ca9ec663d8
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2020-09-09 22:51:02 +0430

    add fetch for receive user data

--- src/routes/home/home.js
+++ src/routes/home/home.js
@@ -1,19 +1,48 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import HeaderContent from '../../components/home/HeaderContent';
 import HomeContent from '../../components/home/HomeContent';
+import {connect} from 'react-redux';
 
 const Home = (props) => {
+  const [information, setInformation] = useState('');
+  const {user} = props;
+
+  const fetchData = async () => {
+    const response = await fetch('https://reqres.in/api/users');
+    const responseJson = await response.json();
+    console.log(responseJson);
+    const data = await responseJson.data;
+    var informationUser = data.find((item) => item.email == user.email);
+    console.log(user, informationUser);
+    if (!informationUser) {
+      response = await fetch('https://reqres.in/api/users?page=2');
+      responseJson = await response.json();
+      console.log(responseJson);
+      data = await responseJson.data;
+      informationUser = data.find((item) => item.email == user.email);
+      setInformation(informationUser);
+    }
+     setInformation(informationUser);
+  };
+  useEffect(() => {
+    fetchData();
+  }, [information]);
   const {navigation} = props;
   return (
     <View style={styles.container}>
-      <HeaderContent />
+      <HeaderContent information={information} />
       <HomeContent navigation={navigation} />
     </View>
   );
 };
+const mapStateToProps = (state) => {
+  return {
+    user: state.user,
+  };
+};
 
-export default Home;
+export default connect(mapStateToProps)(Home);
 
 const styles = StyleSheet.create({
   container: {
